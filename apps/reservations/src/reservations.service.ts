9fee39aee439c7111c013f9f97bb44e34d669e39
hadimakki387 <hmakki387@gmail.com>
2024-02-06 19:20:32 +0200

    implemented crud api for reservations

---
+++ apps/reservations/src/reservations.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { ReservationRepository } from './reservations.repository';
+
+@Injectable()
+export class ReservationsService {
+  constructor(private readonly reservaitonRepository: ReservationRepository) {}
+  create(createReservationDto: CreateReservationDto) {
+    return this.reservaitonRepository.create({
+      ...createReservationDto,
+      timestamp: new Date(),
+      userId: '123',
+    });
+  }
+
+  findAll() {
+    return this.reservaitonRepository.find({});
+  }
+
+  findOne(_id: string) {
+    return this.reservaitonRepository.findOne({ _id });
+  }
+
+  update(_id: string, updateReservationDto: UpdateReservationDto) {
+    return this.reservaitonRepository.findOneAndUpdate(
+      { _id },
+      { $set: updateReservationDto },
+    );
+  }
+
+  remove(_id: string) {
+    return this.reservaitonRepository.findOneAndDelete({ _id });
+  }
+}
