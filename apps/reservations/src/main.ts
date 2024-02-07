2bf9f18839ac6215881765984670c25e86331b80
hadimakki387 <hmakki387@gmail.com>
2024-02-07 16:48:04 +0200

    dockerising the app

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
+  console.log("test 234")
   const app = await NestFactory.create(ReservationsModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
